0af51e89548a8f976202a157baf9aae336114e60
Hari <baluoger91@gmail.com>
2021-08-18 12:33:42 +0530

    some changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,42 @@
-const contents = ["First content" , "Second content", "Third content"]
+/*
+!function (){
+    $("#change-text").Morphext({
+        animation : "fadeIn",
+        seperator : ",",
+        speed : 2000,
+        complete : function(){}
+    });
+}
+*/
+
+var contents = ["First content" , "Second content", "Third content"]
 var i=0
+/*
 for(i=0;i<3;i++){
     a = document.getElementById("change-text")
-    a.innerHTML = contents[i]   
+    var h = contents[i]
+    setTimeout(
+        (h)=>{
+            a.innerHTML = h
+            console.log(h)
+            console.log(a)
+        },2000
+    );
+    //a.innerHTML = contents[i]   
+}
+*/
+setTimeout(
+    ()=>{for(i=0;i<3;i++){
+    a = document.getElementById("change-text")
+    var h = contents[i]
+    task(i)
+    //a.innerHTML = contents[i]   
+}
+    },2000)
+function task(i){
+    console.log(contents[i])
+    setTimeout(function(){
+        a.innerHTML = contents[i]
+        console.log(contents[i])
+    },5000)
 }
