b8ef6b545f8463ecb21c56cfd29831a725c82ffd
avishith <avishithpm@gmail.com>
2021-08-31 01:27:17 +0530

    banner fix , wave fix, gallery added about added

--- sworker.js
+++ sworker.js
@@ -1,16 +1,16 @@
-// self.addEventListener("install",e=>{
-//     console.log("sworker changed..")
-//     e.waitUntil(
-//         caches.open("static").then(cache=>{
-//             return cache.addAll(["./","./css/style.css","./images","./js/script.js","./icons"])
-//         })
-//     )
-// })
+self.addEventListener("install",e=>{
+    console.log("sworker changed..")
+    e.waitUntil(
+        caches.open("static").then(cache=>{
+            return cache.addAll(["./","./css/style.css","./images","./js/script.js","./icons"])
+        })
+    )
+})
 
-// self.addEventListener("fetch",e=>{
-//     e.respondWith(
-//         caches.match(e.request).then(response=>{
-//             return response || fetch(e.request)
-//         })
-//     )
-// })
+self.addEventListener("fetch",e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request)
+        })
+    )
+})
